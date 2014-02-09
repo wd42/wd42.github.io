5b434de2b4efedd9472dc4d6fd7c198d10622043
Jonny Scholes <jonnyscholes@gmail.com>
2014-02-10 00:17:29 +1100

    Added gallery images, styled gallery/form

--- js/main.js
+++ js/main.js
@@ -1,26 +1,15 @@
 (function () {
-  $('.lightswitch').click(function(){
-    $(this).toggleClass('on');
-    $('.contain, .content').toggleClass('open');
+  $('.lightswitch, .top, .bottom').click(function(){
+    $('.lightswitch').toggleClass('on');
+    $('.contain, .inner').toggleClass('open');
     $('body').toggleClass('active');
+    return false;
   });
-//
-//  var swiper = new Swiper('.swiper-container',{
-//    centeredSlides: true,
-//    slidesPerView: 1,
-//    watchActiveIndex: true,
-//    initialSlide: 1,
-//    keyboardControl: true,
-//    cssWidthAndHeight: true
-//  });
 
-//  $('nav a').click(function(){
-//    swiper.swipeTo($(this).data('index'));
-//  });
-
-  var slidey = $('.content').unslider({
-      delay: false
-    });
+  var slidey = $('.inner').unslider({
+    delay: false,
+    keys: false
+  });
 
   var data = slidey.data('unslider'),
     nav = $('nav a');
@@ -30,5 +19,7 @@
     nav.removeClass('active');
     $(this).addClass('active');
   });
+
+  $('.gallery a').colorbox({rel:'gal'});
 
 })();
