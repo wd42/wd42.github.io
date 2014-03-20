bb04863b5117fdc962633d08e4024f5736a16ed9
Jonny Scholes <jonnyscholes@gmail.com>
2014-03-20 14:01:12 +1100

    Changed form styles, updated text, added sponsors, added gmap, fixed gallery styles, added contact methods

--- js/main.js
+++ js/main.js
@@ -20,6 +20,55 @@
     $(this).addClass('active');
   });
 
-  $('.gallery a').colorbox({rel:'gal'});
+  $('.gallery a').colorbox({
+    rel:'gal',
+    current: '',
+    previous: '<span class="icon-angle-left"></span>',
+    next: '<span class="icon-angle-right"></span>',
+    close: '<span class="icon-times">&times;</span>',
+    height: '100%'
+  });
 
+
+  function mapInit() {
+    var styles = [
+      {
+        stylers: [
+          { saturation: -100 },
+          { lightness: -10 }
+        ]
+      },{
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [
+            { lightness: 100 }
+          ]
+        }
+    ],
+    mapOptions = {
+      zoom: 16,
+      center: new google.maps.LatLng(-42.885187,147.336267),
+      streetViewControl: false
+    },
+    contentString = '<div id="content">'+
+      '<p>We meet on the last Wednesday of every month from 5.30pm at:</p>'+
+      '<address>The Typewriter Factory,<br>Level 3, 13-17 Castray Esplanade,<br>Hobart, Tasmania,<br>Australia</address>'+
+      '</div>';
+
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions),
+    infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      maxWidth: 200
+    }),
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(-42.886681,147.336267),
+      map: map,
+      title: 'The Typewriter Factory'
+    });
+
+    map.setOptions({styles: styles});
+    infowindow.open(map,marker);
+  }
+
+  google.maps.event.addDomListener(window, 'load', mapInit);
 })();
